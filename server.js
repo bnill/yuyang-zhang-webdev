95bf4ebf91550e166c525cebf77d60826882ff31
Yuyang Zhang <zhangyy@YuyangmatoMacBook-Pro.local>
2017-07-08 19:16:24 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var app = require('./express');
+var express = app.express;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/public'));
+
+require("./test/app");
+
+//app.listen(3000);
+var port = process.env.PORT || 3000;
+app.listen(port);
