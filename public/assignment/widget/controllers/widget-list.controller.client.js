ab235eaeaeeca1acd2a6bb855d9b7fd705c293f9
Yuyang Zhang <zhangyy@YuyangmatoMacBook-Pro.local>
2017-08-09 20:31:56 -0400

    widget_list_controller update

--- public/assignment/widget/controllers/widget-list.controller.client.js
+++ public/assignment/widget/controllers/widget-list.controller.client.js
@@ -3,16 +3,36 @@
         .module("WamApp")
         .controller("widgetListController", widgetListController);
 
-    function widgetListController($routeParams, widgetService, $location){
+    function widgetListController($routeParams, widgetService, $location, $sce){
         var model = this;
         model.userId = $routeParams.userId;
         model.wid = $routeParams.wid;
         model.pid = $routeParams.pid;
+        model.widgetViewUrl = widgetViewUrl;
+        model.getYoutubeVideo = getYoutubeVideo;
+        model.trust_html = trust_html;
 
         function init(){
             model.widgets = widgetService.findWidgetsByPageId(model.pid);
             //console.log(model.widgets);
         }
         init();
+
+        function widgetViewUrl(widget){
+            var url = 'widget/templates/views/widget-'+widget.widgetType.toLowerCase()+'.view.client.html';
+            //console.log(url);
+            return url;
+        }
+
+        function getYoutubeVideo(url){
+            var embedUrl = "https://www.youtube.com/embed/";
+            var linkUrlParts = url.split('/');
+            embedUrl += url[url.length - 1];
+            return $sce.trustAsResourceUrl(embedUrl);
+        }
+
+        function trust_html(html){
+            return  $sce.trustAsHtml(html);
+        }
     }
 })();
