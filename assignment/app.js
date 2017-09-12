75094785c47d5ceb270e401a547b71e77641d5f7
Yuyang Zhang <zhangyy@YuyangmatoMacBook-Pro.local>
2017-09-11 21:44:39 -0400

    adjustment for deployment

--- assignment/app.js
+++ assignment/app.js
@@ -1,3 +1,18 @@
+var app = require('../express');
+var q = require('q');
+
+var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds151222.mlab.com:51222/heroku_lgcnhhhs'; // user yours
+}
+
+var mongoose = require("mongoose");
+mongoose.connect(connectionString);
+mongoose.Promise = q.Promise;
+
 require("./services/user.service.server");
 require("./services/website.service.server");
 require("./services/page.service.server");
