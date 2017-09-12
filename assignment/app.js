1b7ec3e381580cb718ef06f848870403096643ed
Yuyang Zhang <zhangyy@YuyangmatoMacBook-Pro.local>
2017-09-11 21:49:07 -0400

    adjustment for deployment

--- assignment/app.js
+++ assignment/app.js
@@ -1,7 +1,8 @@
 var app = require('../express');
 var q = require('q');
 
-var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+//var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+var connectionString = "";
 if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
